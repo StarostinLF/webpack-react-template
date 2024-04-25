5cc02e605722ab887a0293b123ed79daa2fb5c08
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2024-04-25 18:40:49 +0300

    refactor(app.tsx): компонент App перенесён в отдельную папку

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
-import App from './components/app'
+import App from './app/app'
 import './assets/styles/globals.scss'
 
 const domNode = document.querySelector('#root') as HTMLDivElement,
