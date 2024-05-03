eac256127044fcc3238269698325d5509d189dca
StarostinLF <avatar5919@mail.ru>
2024-05-03 22:53:33 +0300

    refactor(hooks/store.ts): перенесены типизации хуков Redux в общий файл стора
    Перенесены типизации Redux-хуков useSelector и useDispatch в файл стора services/store.ts

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,5 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import {
+	useDispatch as dispatchHook,
+	useSelector as selectorHook,
+} from 'react-redux'
+import type { TypedUseSelectorHook } from 'react-redux'
 
 export const store = configureStore({
 	reducer: {},
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useDispatch: () => AppDispatch = dispatchHook
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook
